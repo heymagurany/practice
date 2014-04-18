7b6775caf030bd0c465c8c0ecc6ea54570f6535f
Matt Magurany <mmagurany@live.com>
2014-04-18 12:49:09 -0400

    Initial commit of Google Maps practice project

---
+++ GoogleMaps/GoogleMaps/Controllers/HomeController.cs
@@ -0,0 +1,17 @@
+﻿using System.Web.Mvc;
+
+namespace GoogleMaps.Controllers
+{
+    public class HomeController : Controller
+    {
+        public ActionResult Index()
+        {
+            return View();
+        }
+
+        public ActionResult Arc()
+        {
+            return View();
+        }
+	}
+}
