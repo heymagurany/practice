7b5634e49388cef33ee7bc2b6a217da25a2d6184
Matt Magurany <mmagurany@live.com>
2014-04-18 16:28:40 -0400

    Move arc code to a component. Allow user to specify pivot arm length

---
+++ GoogleMaps/GoogleMaps/Scripts/optmzr.maps.extensions.js
@@ -0,0 +1,58 @@
+﻿/* Based the on the Latitude/longitude spherical geodesy formulae & scripts
+  at http://www.movable-type.co.uk/scripts/latlong.html
+  (c) Chris Veness 2002-2010
+*/
+google.maps.LatLng.prototype.DestinationPoint = function (brng, dist) {
+    var radius = 6378137.0; // earth's mean radius in meters
+    var brngRads = brng.toRad();
+    var lat1 = this.lat().toRad(), lon1 = this.lng().toRad();
+    var lat2 = Math.asin(Math.sin(lat1) * Math.cos(dist / radius) + Math.cos(lat1) * Math.sin(dist / radius) * Math.cos(brngRads));
+    var lon2 = lon1 + Math.atan2(Math.sin(brngRads) * Math.sin(dist / radius) * Math.cos(lat1), Math.cos(dist / radius) - Math.sin(lat1) * Math.sin(lat2));
+
+    return new google.maps.LatLng(lat2.toDeg(), lon2.toDeg());
+}
+
+// === A function which returns the bearing between two LatLng in radians ===
+// === If v1 is null, it returns the bearing between the first and last vertex ===
+// === If v1 is present but v2 is null, returns the bearing from v1 to the next vertex ===
+// === If either vertex is out of range, returns void ===
+google.maps.LatLng.prototype.Bearing = function (otherLatLng) {
+    var from = this;
+    var to = otherLatLng;
+    if (from.equals(to)) {
+        return 0;
+    }
+    var lat1 = from.latRadians();
+    var lon1 = from.lngRadians();
+    var lat2 = to.latRadians();
+    var lon2 = to.lngRadians();
+    var angle = -Math.atan2(Math.sin(lon1 - lon2) * Math.cos(lat2), Math.cos(lat1) * Math.sin(lat2) - Math.sin(lat1) * Math.cos(lat2) * Math.cos(lon1 - lon2));
+    if (angle < 0.0) {
+        angle += Math.PI * 2.0;
+    }
+    if (angle > Math.PI) {
+        angle -= Math.PI * 2.0;
+    }
+    return parseFloat(angle.toDeg());
+}
+
+/**
+ * Extend the Number object to convert degrees to radians
+ */
+Number.prototype.toRad = function () {
+    return this * Math.PI / 180;
+};
+
+/**
+ * Extend the Number object to convert radians to degrees
+ */
+Number.prototype.toDeg = function () {
+    return this * 180 / Math.PI;
+};
+
+/**
+ * Normalize a heading in degrees to between 0 and +360
+ */
+Number.prototype.toBrng = function () {
+    return (this.toDeg() + 360) % 360;
+};
---
+++ GoogleMaps/GoogleMaps/Scripts/optmzr.maps.irrigation.js
@@ -0,0 +1,204 @@
+﻿function IrrigationControl(map) {
+    // Create a div to hold the control.
+    var controlDiv = document.createElement('div');
+
+    // Set CSS styles for the DIV containing the control
+    // Setting padding to 5 px will offset the control
+    // from the edge of the map.
+    controlDiv.style.padding = '5px';
+
+    var $radiusInput = $("<input type='text' />");
+    var $radiusUnitsInput = $("<select><option value='3.2808'>feet</options><option value='1'>meters</options></select>");
+    var $radiusContainer = $("<div style='background-color:white;border-style:solid;border-width:2px;padding-left:4px;padding-right:4px'>Arm Length: </div>").append($radiusInput, $radiusUnitsInput);
+
+    controlDiv.appendChild($radiusContainer[0]);
+
+    // Set CSS for the control border.
+    var addButton = function (text, onClick) {
+        var controlText = document.createElement('div');
+        controlText.style.fontFamily = 'Arial,sans-serif';
+        controlText.style.fontSize = '12px';
+        controlText.style.paddingLeft = '4px';
+        controlText.style.paddingRight = '4px';
+        controlText.innerHTML = '<strong>' + text + '</strong>';
+
+        var controlUI = document.createElement('div');
+        controlUI.style.backgroundColor = 'white';
+        controlUI.style.borderStyle = 'solid';
+        controlUI.style.borderWidth = '2px';
+        controlUI.style.cursor = 'pointer';
+        controlUI.style.textAlign = 'center';
+        controlUI.title = 'Click to set the map to Home';
+        controlUI.appendChild(controlText);
+
+        controlDiv.appendChild(controlUI);
+
+        google.maps.event.addDomListener(controlUI, "click", onClick);
+    }
+
+    this.getElement = function () {
+        return controlDiv;
+    }
+
+    var clickCount = 0;
+    var startPoint;
+    var startMarker;
+    var endPoint;
+    var endMarker;
+    var centerPoint;
+    var centerMarker;
+    var _radius;
+    var arc;
+    var infowindow = new google.maps.InfoWindow(
+    {
+        size: new google.maps.Size(150, 50)
+    });
+
+    $radiusContainer.change(function (e) {
+        _radius = parseFloat($radiusInput.val()) / parseFloat($radiusUnitsInput.val());
+
+        drawArc();
+    });
+
+    addButton("Clear Irrigation", function () {
+        var objects = [
+            arc,
+            startMarker,
+            endMarker,
+            centerMarker
+        ];
+
+        for (var i = 0; i < objects.length; i++) {
+            if (objects[i]) {
+                objects[i].set("map", null);
+            }
+        }
+
+        clickCount = 0;
+    });
+
+    var createMarker = function (latlng, html) {
+        var contentString = html;
+        var marker = new google.maps.Marker({
+            draggable: true,
+            position: latlng,
+            map: map,
+            zIndex: Math.round(latlng.lat() * -100000) << 5
+        });
+        //bounds.extend(latlng);
+        google.maps.event.addListener(marker, 'click', function () {
+            infowindow.setContent(contentString);
+            infowindow.open(map, marker);
+        });
+        return marker;
+    }
+
+    var createArcVertices = function (center, start, end, direction) {
+        var initialBearing = center.Bearing(start);
+        var finalBearing = center.Bearing(end);
+        var radius;
+        var points = 32;
+        var extp = new Array();
+
+        if (_radius) {
+            radius = _radius;
+        } else {
+            var distanceFromStart = center.distanceFrom(start);
+            var distanceFromEnd = center.distanceFrom(end);
+
+            if (distanceFromStart > distanceFromEnd) {
+                radius = distanceFromStart;
+            } else {
+                radius = distanceFromEnd;
+            }
+        }
+
+        if (initialBearing > finalBearing) {
+            if (direction > 0) {
+                finalBearing += 360;
+            }
+        } else {
+            if (direction < 0) {
+                finalBearing -= 360;
+            }
+        }
+
+        var deltaBearing = (finalBearing - initialBearing) / points;
+        var maxBearing = 5;
+
+        while ((deltaBearing < -maxBearing) || (deltaBearing > maxBearing)) {
+            points *= 2;
+            deltaBearing = (finalBearing - initialBearing) / points;
+        }
+
+        for (var i = 0; i <= points; i++) {
+            var point = center.DestinationPoint(initialBearing + i * deltaBearing, radius);
+
+            extp.push(point);
+        }
+
+        extp.push(center);
+        extp.push(center.DestinationPoint(initialBearing, radius));
+
+        return extp;
+    }
+
+    var drawArc = function () {
+        if (startPoint && endPoint && centerPoint) {
+            if (arc) {
+                arc.setMap(null);
+            }
+
+            var arcPts = createArcVertices(centerPoint, startPoint, endPoint, 1);
+
+            arc = new google.maps.Polygon({
+                paths: [arcPts],
+                strokeColor: "#0000FF",
+                strokeOpacity: 0.5,
+                strokeWeight: 2,
+                fillColor: "#0000FF",
+                fillOpacity: 0.35,
+                map: map
+            });
+        }
+
+        return arc;
+    }
+
+    google.maps.event.addListener(map, "click", function (e) {
+        if (clickCount == 0) {
+            centerPoint = e.latLng;
+            centerMarker = createMarker(centerPoint, "Center");
+
+            google.maps.event.addListener(centerMarker, "dragend", function (e) {
+                centerPoint = e.latLng;
+
+                arc = drawArc();
+            });
+        } else if (clickCount == 1) {
+            startPoint = e.latLng;
+            startMarker = createMarker(startPoint, "Start");
+
+            google.maps.event.addListener(startMarker, "dragend", function (e) {
+                startPoint = e.latLng;
+
+                arc = drawArc();
+            });
+        } else if (clickCount == 2) {
+            endPoint = e.latLng;
+            endMarker = createMarker(endPoint, "End");
+
+            google.maps.event.addListener(endMarker, "dragend", function (e) {
+                endPoint = e.latLng;
+
+                arc = drawArc();
+            });
+
+            arc = drawArc();
+        } else {
+            infowindow.close();
+        }
+
+        clickCount++;
+    });
+}
