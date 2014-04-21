f05fd0879f6ad77478f025d692866d0a435eb0d8
Matt Magurany <mmagurany@live.com>
2014-04-21 16:50:37 -0400

    Update irrigation control to be dual-bound to input fields.

--- GoogleMaps/GoogleMaps/Scripts/optmzr.maps.irrigation.js
+++ GoogleMaps/GoogleMaps/Scripts/optmzr.maps.irrigation.js
@@ -1,231 +1,231 @@
-﻿function IrrigationControl(map) {
-    // Create a div to hold the control.
-    var $radiusInput = $("<input type='text' />");
-    var $radiusUnitsInput = $("<select><option value='3.2808'>feet</options><option value='1'>meters</options></select>");
-    var $radiusContainer = $("<div style='background-color:white;border-style:solid;border-width:2px;padding-left:4px;padding-right:4px'>Arm Length: </div>").append($radiusInput, $radiusUnitsInput);
+﻿var optmzr = (function () {
+    var optmzr = {};
 
-    var $centerLatInput = $("<input type='text' />");
-    var $centerLngInput = $("<input type='text' />");
-    var $centerContainer = $("<div style='background-color:white;border-style:solid;border-width:2px;padding-left:4px;padding-right:4px'>Center: </div>").append($centerLatInput, $centerLngInput);
+    optmzr.IrrigationControl = function (map) {
+        var $radiusInput = $("<input type='text' />");
+        var $radiusUnitsInput = $("<select><option value='3.2808'>feet</options><option value='1'>meters</options></select>");
+        var $radiusContainer = $("<div style='background-color:white;border-style:solid;border-width:2px;padding-left:4px;padding-right:4px'>Arm Length: </div>").append($radiusInput, $radiusUnitsInput);
 
-    var $startBearingInput = $("<input type='text' />");
-    var $startBearingContainer = $("<div style='background-color:white;border-style:solid;border-width:2px;padding-left:4px;padding-right:4px'>Start Bearing: </div>").append($startBearingInput);
+        var $centerLatInput = $("<input type='text' />");
+        var $centerLngInput = $("<input type='text' />");
+        var $centerContainer = $("<div style='background-color:white;border-style:solid;border-width:2px;padding-left:4px;padding-right:4px'>Center: </div>").append($centerLatInput, $centerLngInput);
 
-    var $endBearingInput = $("<input type='text' />");
-    var $endBearingContainer = $("<div style='background-color:white;border-style:solid;border-width:2px;padding-left:4px;padding-right:4px'>End Bearing: </div>").append($endBearingInput);
+        var $startBearingInput = $("<input type='text' />");
+        var $startBearingContainer = $("<div style='background-color:white;border-style:solid;border-width:2px;padding-left:4px;padding-right:4px'>Start Bearing: </div>").append($startBearingInput);
 
-    var $control = $("<div style='padding:5px'></div>").append($radiusContainer, $centerContainer, $startBearingContainer, $endBearingContainer);
+        var $endBearingInput = $("<input type='text' />");
+        var $endBearingContainer = $("<div style='background-color:white;border-style:solid;border-width:2px;padding-left:4px;padding-right:4px'>End Bearing: </div>").append($endBearingInput);
 
-    // Set CSS for the control border.
-    var addButton = function (text, onClick) {
-        var $text = $("<div style='font-family:Arial,sans-serif;font-size:12px;padding-left:4px;padding-right:4px;'></div>").html(text);
-        var $button = $("<div style='background-color:white;border-style:solid;border-width:2px;cursor:pointer;text-align:center'></div>").append($text);
+        var $clearButton = $("<div style='background-color:white;border-style:solid;border-width:2px;cursor:pointer;text-align:center'><div style='font-family:Arial,sans-serif;font-size:12px;padding-left:4px;padding-right:4px;'>Clear Irrigation</div></div>");
 
-        $control.append($button);
+        var $control = $("<div style='padding:5px'></div>").append($radiusContainer, $centerContainer, $startBearingContainer, $endBearingContainer, $clearButton);
 
-        google.maps.event.addDomListener($button[0], "click", onClick);
-    }
-
-    this.getElement = function () {
-        return $control[0];
-    }
-
-    var clickCount = 0;
-    var startPoint;
-    var startMarker;
-    var endPoint;
-    var endMarker;
-    var centerPoint;
-    var centerMarker;
-    var radius;
-    var arc;
-    var infowindow = new google.maps.InfoWindow(
-    {
-        size: new google.maps.Size(150, 50)
-    });
-
-    $radiusContainer.change(function () {
-        radius = parseFloat($radiusInput.val()) / parseFloat($radiusUnitsInput.val());
-
-        drawArc();
-    });
-
-    $centerLatInput.change(function () {
-        if(centerMarker) {
-            centerMarker.setMap(null);
+        this.getElement = function () {
+            return $control[0];
         }
 
-        centerPoint = new google.maps.LatLng(parseFloat($centerLatInput.val()), centerPoint.lng());
-        centerMarker = createMarker(centerPoint, "Center");
-        arc = drawArc();
-    });
+        var clickCount = 0;
+        var startMarker;
+        var endMarker;
+        var centerMarker;
+        var radius;
+        var arc;
+        var infowindow = new google.maps.InfoWindow({
+            size: new google.maps.Size(150, 50)
+        });
 
-    $centerLngInput.change(function () {
-        if(centerMarker) {
-            centerMarker.setMap(null);
-        }
+        $radiusContainer.change(function () {
+            radius = parseFloat($radiusInput.val()) / parseFloat($radiusUnitsInput.val());
 
-        centerPoint = new google.maps.LatLng(centerPoint.lat(), parseFloat($centerLngInput.val()));
-        centerMarker = createMarker(centerPoint, "Center");
-        arc = drawArc();
-    });
+            drawArc();
+        });
 
-    $startBearingInput.change(function () {
-        if(startMarker) {
-            startMarker.setMap(null);
-        }
+        $centerLatInput.change(function () {
+            var point = new google.maps.LatLng(parseFloat($centerLatInput.val()), centerMarker.getPosition().lng());
+            centerMarker.setPosition(point);
+        });
 
+        $centerLngInput.change(function () {
+            var point = new google.maps.LatLng(centerMarker.getPosition().lat(), parseFloat($centerLngInput.val()));
+            centerMarker.setPosition(point);
+        });
 
-    });
-
-    addButton("Clear Irrigation", function () {
-        var objects = [
-            arc,
-            startMarker,
-            endMarker,
-            centerMarker
-        ];
-
-        for (var i = 0; i < objects.length; i++) {
-            if (objects[i]) {
-                objects[i].set("map", null);
+        $startBearingInput.change(function () {
+            if (startMarker) {
+                startMarker.setMap(null);
             }
+            var bearing = centerMarker.getPosition().Bearing(startMarker.getPosition());
+
+        });
+
+        $endBearingInput.change(function () {
+            if (endMarker) {
+                endMarker.setMap(null);
+            }
+        });
+
+        var createMarker = function (latlng, html) {
+            var contentString = html;
+            var marker = new google.maps.Marker({
+                draggable: true,
+                position: latlng,
+                map: map,
+                zIndex: Math.round(latlng.lat() * -100000) << 5
+            });
+            google.maps.event.addListener(marker, 'click', function () {
+                infowindow.setContent(contentString);
+                infowindow.open(map, marker);
+            });
+            return marker;
         }
 
-        clickCount = 0;
+        var createArcVertices = function (center, initialBearing, finalBearing, radius, direction) {
+            var points = 32;
+            var extp = new Array();
 
-        $centerLatInput.val("");
-        $centerLngInput.val("");
-        $startBearingInput.val("");
-        $endBearingInput.val("");
-    });
-
-    var createMarker = function (latlng, html) {
-        var contentString = html;
-        var marker = new google.maps.Marker({
-            draggable: true,
-            position: latlng,
-            map: map,
-            zIndex: Math.round(latlng.lat() * -100000) << 5
-        });
-        google.maps.event.addListener(marker, 'click', function () {
-            infowindow.setContent(contentString);
-            infowindow.open(map, marker);
-        });
-        return marker;
-    }
-
-    var createArcVertices = function (center, start, end, direction) {
-        var initialBearing = center.Bearing(start);
-        var finalBearing = center.Bearing(end);
-        var useRadius;
-        var points = 32;
-        var extp = new Array();
-
-        if (radius) {
-            useRadius = radius;
-        } else {
-            var distanceFromStart = center.distanceFrom(start);
-            var distanceFromEnd = center.distanceFrom(end);
-
-            if (distanceFromStart > distanceFromEnd) {
-                useRadius = distanceFromStart;
+            if (initialBearing > finalBearing) {
+                if (direction > 0) {
+                    finalBearing += 360;
+                }
             } else {
-                useRadius = distanceFromEnd;
+                if (direction < 0) {
+                    finalBearing -= 360;
+                }
             }
-        }
 
-        if (initialBearing > finalBearing) {
-            if (direction > 0) {
-                finalBearing += 360;
+            var deltaBearing = (finalBearing - initialBearing) / points;
+            var maxBearing = 5;
+
+            while ((deltaBearing < -maxBearing) || (deltaBearing > maxBearing)) {
+                points *= 2;
+                deltaBearing = (finalBearing - initialBearing) / points;
             }
-        } else {
-            if (direction < 0) {
-                finalBearing -= 360;
+
+            for (var i = 0; i <= points; i++) {
+                var point = center.DestinationPoint(initialBearing + i * deltaBearing, radius);
+
+                extp.push(point);
             }
+
+            extp.push(center);
+            extp.push(center.DestinationPoint(initialBearing, radius));
+
+            return extp;
         }
 
-        var deltaBearing = (finalBearing - initialBearing) / points;
-        var maxBearing = 5;
+        var drawArc = function () {
+            if (centerMarker) {
+                var center = centerMarker.getPosition();
 
-        while ((deltaBearing < -maxBearing) || (deltaBearing > maxBearing)) {
-            points *= 2;
-            deltaBearing = (finalBearing - initialBearing) / points;
+                $centerLatInput.val(center.lat());
+                $centerLngInput.val(center.lng());
+
+                if (startMarker) {
+                    var start = startMarker.getPosition();
+                    var initialBearing = center.Bearing(start);
+                    var useRadius;
+
+                    $startBearingInput.val(initialBearing);
+
+                    if (endMarker) {
+                        var end = endMarker.getPosition();
+                        var finalBearing = center.Bearing(end);
+
+                        if (radius) {
+                            useRadius = radius;
+                        } else {
+                            var distanceFromStart = center.distanceFrom(start);
+                            var distanceFromEnd = center.distanceFrom(end);
+
+                            if (distanceFromStart > distanceFromEnd) {
+                                useRadius = distanceFromStart;
+                            } else {
+                                useRadius = distanceFromEnd;
+                            }
+                        }
+
+                        $endBearingInput.val(finalBearing);
+
+                        var arcPts = createArcVertices(center, initialBearing, finalBearing, useRadius, optmzr.IrrigationDirection.CLOCKWISE);
+
+                        if (arc) {
+                            arc.setMap(null);
+                        }
+
+                        arc = new google.maps.Polygon({
+                            paths: [arcPts],
+                            strokeColor: "#0000FF",
+                            strokeOpacity: 0.5,
+                            strokeWeight: 2,
+                            fillColor: "#0000FF",
+                            fillOpacity: 0.35,
+                            map: map
+                        });
+                    } else {
+                        if (radius) {
+                            useRadius = radius;
+                        } else {
+                            useRadius = center.distanceFrom(start);
+                        }
+                    }
+
+                    $radiusInput.val(useRadius * parseFloat($radiusUnitsInput.val()));
+                }
+            }
+
+            return arc;
         }
 
-        for (var i = 0; i <= points; i++) {
-            var point = center.DestinationPoint(initialBearing + i * deltaBearing, useRadius);
+        google.maps.event.addListener(map, "click", function (e) {
+            if (clickCount == 0) {
+                centerMarker = createMarker(e.latLng, "Center");
 
-            extp.push(point);
-        }
+                google.maps.event.addListener(centerMarker, "position_changed", drawArc);
+            } else if (clickCount == 1) {
+                startMarker = createMarker(e.latLng, "Start");
 
-        extp.push(center);
-        extp.push(center.DestinationPoint(initialBearing, useRadius));
+                google.maps.event.addListener(startMarker, "position_changed", drawArc);
+            } else if (clickCount == 2) {
+                endMarker = createMarker(e.latLng, "End");
 
-        return extp;
+                google.maps.event.addListener(endMarker, "position_changed", drawArc);
+
+                arc = drawArc();
+            } else {
+                infowindow.close();
+            }
+
+            clickCount++;
+        });
+
+        google.maps.event.addDomListener($clearButton[0], "click", function () {
+            var objects = [
+                arc,
+                startMarker,
+                endMarker,
+                centerMarker
+            ];
+
+            for (var i = 0; i < objects.length; i++) {
+                if (objects[i]) {
+                    objects[i].set("map", null);
+                }
+            }
+
+            clickCount = 0;
+
+            $radiusUnitsInput.val("");
+            $centerLatInput.val("");
+            $centerLngInput.val("");
+            $startBearingInput.val("");
+            $endBearingInput.val("");
+        });
     }
 
-    var drawArc = function () {
-        if (startPoint && endPoint && centerPoint) {
-            if (arc) {
-                arc.setMap(null);
-            }
+    optmzr.IrrigationDirection = {
+        CLOCKWISE: 1,
+        COUNTER_CLOCKWISE: 1
+    };
 
-            var arcPts = createArcVertices(centerPoint, startPoint, endPoint, 1);
-
-            arc = new google.maps.Polygon({
-                paths: [arcPts],
-                strokeColor: "#0000FF",
-                strokeOpacity: 0.5,
-                strokeWeight: 2,
-                fillColor: "#0000FF",
-                fillOpacity: 0.35,
-                map: map
-            });
-        }
-
-        return arc;
-    }
-
-    google.maps.event.addListener(map, "click", function (e) {
-        if (clickCount == 0) {
-            centerPoint = e.latLng;
-            centerMarker = createMarker(centerPoint, "Center");
-            $centerLatInput.val(centerPoint.lat());
-            $centerLngInput.val(centerPoint.lng());
-
-            google.maps.event.addListener(centerMarker, "dragend", function (e) {
-                centerPoint = e.latLng;
-                $centerLatInput.val(centerPoint.lat());
-                $centerLngInput.val(centerPoint.lng());
-                arc = drawArc();
-            });
-        } else if (clickCount == 1) {
-            startPoint = e.latLng;
-            startMarker = createMarker(startPoint, "Start");
-            $startBearingInput.val(centerPoint.Bearing(startPoint));
-
-            google.maps.event.addListener(startMarker, "dragend", function (e) {
-                startPoint = e.latLng;
-                $startBearingInput.val(centerPoint.Bearing(startPoint));
-                arc = drawArc();
-            });
-        } else if (clickCount == 2) {
-            endPoint = e.latLng;
-            endMarker = createMarker(endPoint, "End");
-            $endBearingInput.val(centerPoint.Bearing(endPoint));
-
-            google.maps.event.addListener(endMarker, "dragend", function (e) {
-                endPoint = e.latLng;
-                $endBearingInput.val(centerPoint.Bearing(endPoint));
-                arc = drawArc();
-            });
-
-            arc = drawArc();
-        } else {
-            infowindow.close();
-        }
-
-        clickCount++;
-    });
-}
+    return optmzr;
+}());
