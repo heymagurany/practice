c6cff77a1dc10caaacf1ff30e4f2f97c567eb8f3
Matt Magurany <mmagurany@live.com>
2014-04-22 20:02:51 -0400

    Add save button that triggers a save event for consumers of the control

--- GoogleMaps/GoogleMaps/Scripts/optmzr.maps.irrigation.js
+++ GoogleMaps/GoogleMaps/Scripts/optmzr.maps.irrigation.js
@@ -6,11 +6,11 @@
         var extp = new Array();
 
         if (initialBearing > finalBearing) {
-            if (direction > 0) {
+            if (direction == optmzr.IrrigationDirection.CLOCKWISE) {
                 finalBearing += 360;
             }
         } else {
-            if (direction < 0) {
+            if (direction == optmzr.IrrigationDirection.COUNTER_CLOCKWISE) {
                 finalBearing -= 360;
             }
         }
@@ -66,14 +66,17 @@
         var $endBearingOff = $("<a href='#'>Nevermind</a>").hide();
         var $endBearingContainer = $("<div style='background-color:white;border-style:solid;border-width:2px;padding-left:4px;padding-right:4px'>End Bearing: </div>").append($endBearingOn, $endBearingInput, $endBearingOff);
 
-        var $clearButton = $("<div style='background-color:white;border-style:solid;border-width:2px;cursor:pointer;text-align:center'><div style='font-family:Arial,sans-serif;font-size:12px;padding-left:4px;padding-right:4px;'>Clear Irrigation</div></div>");
+        var $clearButton = $("<span style='font-family:Arial,sans-serif;font-size:12px;padding-left:4px;padding-right:4px;'>Clear Irrigation</span>");
+        var $saveButton = $("<span style='font-family:Arial,sans-serif;font-size:12px;padding-left:4px;padding-right:4px;'>Save</span>");
+        var $buttonContainer = $("<div style='background-color:white;border-style:solid;border-width:2px;cursor:pointer;text-align:center'></div>").append($clearButton, $saveButton);
 
-        var $control = $("<div style='padding:5px'></div>").append($radiusContainer, $directionContainer, $centerContainer, $startBearingContainer, $endBearingContainer, $clearButton);
+        var $control = $("<div style='padding:5px'></div>").append($radiusContainer, $directionContainer, $centerContainer, $startBearingContainer, $endBearingContainer, $buttonContainer);
 
         this.getElement = function () {
             return $control[0];
         }
 
+        var self = this;
         var clickCount = 0;
         var radius;
         var direction = optmzr.IrrigationDirection.CLOCKWISE;
@@ -81,7 +84,7 @@
         var startMarker;
         var endEnabled = false;
         var endMarker;
-        var arc;
+        var shape;
         var shapeOptions = {
             strokeColor: "#0000FF",
             strokeOpacity: 0.5,
@@ -138,7 +141,7 @@
             endEnabled = false;
             clickCount--;
 
-            if(endMarker) {
+            if (endMarker) {
                 endMarker.setMap(null);
                 endMarker = null;
             }
@@ -150,123 +153,10 @@
             $endBearingOff.hide();
         });
 
-        var createMarker = function (latlng, html) {
-            var marker = new google.maps.Marker({
-                draggable: true,
-                position: latlng,
-                map: map,
-                zIndex: Math.round(latlng.lat() * -100000) << 5
-            });
-
-            google.maps.event.addListener(marker, 'click', function () {
-                infowindow.setContent(html);
-                infowindow.open(map, marker);
-            });
-
-            google.maps.event.addListener(marker, "position_changed", draw);
-
-            return marker;
-        }
-
-        var draw = function () {
-            if (centerMarker) {
-                var center = centerMarker.getPosition();
-
-                $centerLatInput.val(center.lat());
-                $centerLngInput.val(center.lng());
-
-                if (startMarker) {
-                    var start = startMarker.getPosition();
-                    var initialBearing = google.maps.geometry.spherical.computeHeading(center, start);
-                    var useRadius;
-
-                    $startBearingInput.val(initialBearing);
-
-                    if (endMarker) {
-                        var end = endMarker.getPosition();
-                        var finalBearing = google.maps.geometry.spherical.computeHeading(center, end);
-
-                        if (radius) {
-                            useRadius = radius;
-                        } else {
-                            var distanceFromStart = google.maps.geometry.spherical.computeDistanceBetween(center, start);
-                            var distanceFromEnd = google.maps.geometry.spherical.computeDistanceBetween(center, end);
-
-                            if (distanceFromStart > distanceFromEnd) {
-                                useRadius = distanceFromStart;
-                            } else {
-                                useRadius = distanceFromEnd;
-                            }
-                        }
-
-                        $endBearingInput.val(finalBearing);
-
-                        var arcPts = createArcVertices(center, initialBearing, finalBearing, useRadius, direction);
-
-                        if (arc instanceof google.maps.Polygon) {
-                            arc.setPath(arcPts);
-                        } else {
-                            if (arc) {
-                                arc.setMap(null);
-                            }
-
-                            arc = new google.maps.Polygon($.extend(shapeOptions, {
-                                paths: [arcPts],
-                                map: map
-                            }));
-                        }
-
-                    } else {
-                        if (radius) {
-                            useRadius = radius;
-                        } else {
-                            useRadius = google.maps.geometry.spherical.computeDistanceBetween(center, start);
-                        }
-
-                        if (arc instanceof google.maps.Circle) {
-                            arc.setCenter(center);
-                            arc.setRadius(useRadius);
-                        } else {
-                            if (arc) {
-                                arc.setMap(null);
-                            }
-
-                            arc = new google.maps.Circle($.extend(shapeOptions, {
-                                center: center,
-                                radius: useRadius,
-                                map: map
-                            }));
-                        }
-                    }
-
-                    $radiusInput.val(useRadius * parseFloat($radiusUnitsInput.val()));
-                }
-            }
-        }
-
-        google.maps.event.addListener(map, "click", function (e) {
-            if (clickCount == 0) {
-                clickCount++;
-                centerMarker = createMarker(e.latLng, "Center");
-            } else if (clickCount == 1) {
-                clickCount++;
-                startMarker = createMarker(e.latLng, "Start");
-                draw();
-            } else {
-                if (endEnabled && (clickCount == 2)) {
-                    clickCount++;
-                    endMarker = createMarker(e.latLng, "End");
-                    draw();
-                }
-            }
-
-            infowindow.close();
-        });
-
-        google.maps.event.addDomListener($clearButton[0], "click", function () {
-            if(arc) {
-                arc.setMap(null);
-                arc = null;
+        $clearButton.click(function () {
+            if (shape) {
+                shape.setMap(null);
+                shape = null;
             }
 
             if (startMarker) {
@@ -291,6 +181,153 @@
             $centerLngInput.val("");
             $startBearingInput.val("");
             $endBearingInput.val("");
+        });
+
+        $saveButton.click(function () {
+           google.maps.event.trigger(self, "save", calculateGeometry());
+        });
+
+        var createMarker = function (latlng, html) {
+            var marker = new google.maps.Marker({
+                draggable: true,
+                position: latlng,
+                map: map,
+                zIndex: Math.round(latlng.lat() * -100000) << 5
+            });
+
+            google.maps.event.addListener(marker, 'click', function () {
+                infowindow.setContent(html);
+                infowindow.open(map, marker);
+            });
+
+            google.maps.event.addListener(marker, "position_changed", draw);
+
+            return marker;
+        }
+
+        var calculateGeometry = function () {
+            var data = {
+                direction: direction
+            };
+
+            if (centerMarker) {
+                var centerPoint = centerMarker.getPosition();
+
+                data.center = centerPoint;
+
+                if (startMarker) {
+                    var startPoint = startMarker.getPosition();
+
+                    data.length = google.maps.geometry.spherical.computeDistanceBetween(centerPoint, startPoint);
+                    data.startHeading = google.maps.geometry.spherical.computeHeading(centerPoint, startPoint);
+
+                    if (endMarker) {
+                        var endPoint = endMarker.getPosition();
+
+                        data.endHeading = google.maps.geometry.spherical.computeHeading(centerPoint, endPoint);
+
+                        if (radius) {
+                            data.radius = radius;
+                        } else {
+                            var distanceFromStart = google.maps.geometry.spherical.computeDistanceBetween(centerPoint, startPoint);
+                            var distanceFromEnd = google.maps.geometry.spherical.computeDistanceBetween(centerPoint, endPoint);
+
+                            if (distanceFromStart > distanceFromEnd) {
+                                data.radius = distanceFromStart;
+                            } else {
+                                data.radius = distanceFromEnd;
+                            }
+                        }
+                    } else {
+                        data.endHeading = null;
+
+                        if (radius) {
+                            data.radius = radius;
+                        } else {
+                            data.radius = google.maps.geometry.spherical.computeDistanceBetween(centerPoint, startPoint);
+                        }
+                    }
+                } else {
+                    data.length = 0;
+                    data.startHeading = null;
+                    data.endHeading = null;
+                }
+            } else {
+                data.length = 0;
+                data.center = null;
+                data.startHeading = null;
+                data.endHeading = null;
+            }
+
+            return data;
+        }
+
+        var draw = function () {
+            var geometry = calculateGeometry();
+
+            if (geometry.center) {
+                $centerLatInput.val(geometry.center.lat());
+                $centerLngInput.val(geometry.center.lng());
+
+                if (geometry.startHeading) {
+                    $radiusInput.val(geometry.radius * parseFloat($radiusUnitsInput.val()));
+                    $startBearingInput.val(geometry.startHeading);
+
+                    if (endMarker) {
+                        $endBearingInput.val(geometry.endHeading);
+
+                        var arcPts = createArcVertices(geometry.center, geometry.startHeading, geometry.endHeading, geometry.radius, geometry.direction);
+
+                        if (shape instanceof google.maps.Polygon) {
+                            shape.setPath(arcPts);
+                        } else {
+                            if (shape) {
+                                shape.setMap(null);
+                            }
+
+                            shape = new google.maps.Polygon($.extend(shapeOptions, {
+                                paths: [arcPts],
+                                map: map
+                            }));
+                        }
+
+                    } else {
+                        if (shape instanceof google.maps.Circle) {
+                            shape.setCenter(geometry.center);
+                            shape.setRadius(geometry.radius);
+                        } else {
+                            if (shape) {
+                                shape.setMap(null);
+                            }
+
+                            shape = new google.maps.Circle($.extend(shapeOptions, {
+                                center: geometry.center,
+                                radius: geometry.radius,
+                                map: map
+                            }));
+                        }
+                    }
+                }
+            }
+        }
+
+        google.maps.event.addListener(map, "click", function (e) {
+            if (clickCount == 0) {
+                clickCount++;
+                centerMarker = createMarker(e.latLng, "Center");
+            } else if (clickCount == 1) {
+                clickCount++;
+                startMarker = createMarker(e.latLng, "Start");
+                draw();
+            } else {
+                if (endEnabled && (clickCount == 2)) {
+                    clickCount++;
+                    endMarker = createMarker(e.latLng, "End");
+                    draw();
+                }
+            }
+
+            infowindow.close();
         });
     }
 
