8c7bb2a41a736ec82fe901da3c7874b08a44daaf
Matt Magurany <mmagurany@live.com>
2014-09-22 20:33:39 -0400

    Add missing file

---
+++ CommanderClientDemo/WebUI/Models/SimpleWebToken.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using Newtonsoft.Json;
+
+namespace WebUI.Models
+{
+    internal sealed class SimpleWebToken
+    {
+        [JsonProperty("access_token")]
+        public string AccessToken { get; set; }
+
+        [JsonProperty("token_type")]
+        public string TokenType { get; set; }
+
+        [JsonProperty("expires_in")]
+        public int ExpiresIn { get; set; }
+
+        public DateTime ExpiresOn { get; set; }
+
+        [JsonProperty("refresh_token")]
+        public string RefreshToken { get; set; }
+    }
+}
