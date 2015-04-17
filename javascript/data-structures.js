5a94349c813e4f5ec6d819f1717cd6659cde58bc
Matt Magurany <mmagurany@live.com>
2015-04-16 22:46:59 -0400

    Tree practice

--- javascript/data-structures.js
+++ javascript/data-structures.js
@@ -4,9 +4,8 @@ function Node (data) {
   this.right = null;
 }
 module.exports = {
-  Node: Node,
   BinaryTree: function () {
-    this.insert = function(root, data) {
+    this.insert = function (root, data) {
       if (!root) {
         return new Node(data);
       }
@@ -15,6 +14,82 @@ module.exports = {
       }
       else {
         root.right = this.insert(root.right, data);
+      }
+      return root;
+    };
+    this.find = function (root, data) {
+      if (!root || root.data === data) {
+        return root;
+      }
+      if (data < root.data) {
+        return this.find(root.left, data);
+      }
+      return this.find(root.right, data);
+    };
+    this.findParent = function (root, target) {
+      if (root == target) {
+        return null;
+      }
+      while (root && root.left != target && root.right != target) {
+        if (target.data < root.data) {
+          root = root.left;
+        }
+        else {
+          root = root.right;
+        }
+      }
+      return root;
+    };
+    this.findSuccessor = function (root, target) {
+      var successor = target.right;
+      // There is a right child, so the right child's deepest left node will
+      // be the successor.
+      if (successor) {
+        while (successor.left) {
+          successor = successor.left;
+        }
+        return successor;
+      }
+      // There isn't a right child, so find the target's ancestor where the
+      // target is in the left sub-tree.
+      do {
+        if (successor) {
+          target = successor;
+        }
+        successor = this.findParent(root, target);
+      } while (successor && successor.right == target);
+      return successor;
+    };
+    this.remove = function (root, target) {
+      if (target.left != null && target.right != null) {
+        var successor = this.findSuccessor(root, target);
+        this.remove(root, successor);
+        target.data = successor.data;
+        return root;
+      }
+      if (target.left != null) {
+        var left = target.left;
+        target.data = left.data;
+        target.left = left.left;
+        target.right = left.right;
+        return root;
+      }
+      if (target.right != null) {
+        var right = target.right;
+        target.data = right.data;
+        target.left = right.left;
+        target.right = right.right;
+        return root;
+      }
+      if (root == target) {
+        return null;
+      }
+      var parent = this.findParent(root, target);
+      if (parent.left == target) {
+        parent.left = null;
+      }
+      else {
+        parent.right = null;
       }
       return root;
     };
--- javascript/tree-tests.js
+++ javascript/tree-tests.js
@@ -1,10 +1,63 @@
 var DataStructures = require('./data-structures.js');
 
 var tree1 = new DataStructures.BinaryTree();
-var root1 = tree1.insert(null, 5);
-root1 = tree1.insert(root1, 7);
-root1 = tree1.insert(root1, 3);
-root1 = tree1.insert(root1, 9);
-root1 = tree1.insert(root1, 1);
+
+console.log('Insert:');
+var root1 = tree1.insert(null, 4);
+root1 = tree1.insert(root1, 2);
 root1 = tree1.insert(root1, 6);
+root1 = tree1.insert(root1, 1);
+root1 = tree1.insert(root1, 3);
+root1 = tree1.insert(root1, 5);
+root1 = tree1.insert(root1, 7);
+root1 = tree1.insert(root1, 8);
 console.log(tree1.toString(root1));
+console.log();
+
+console.log('Find:');
+var node4 = tree1.find(root1, 4);
+var node1 = tree1.find(root1, 1);
+var node6 = tree1.find(root1, 6);
+var node7 = tree1.find(root1, 7);
+console.log('Root: ' + node4.data);
+console.log('No Children: ' + node1.data);
+console.log('One Child: ' + node7.data);
+console.log('Two Children: ' + node6.data);
+console.log();
+
+console.log('Find Parent:');
+var parent4 = tree1.findParent(root1, node4);
+var parent1 = tree1.findParent(root1, node1);
+var parent6 = tree1.findParent(root1, node6);
+var parent7 = tree1.findParent(root1, node7);
+console.log('Parent 4: ' + (parent4 ? parent4.data : '(null)'));
+console.log('Parent 1: ' + parent1.data);
+console.log('Parent 6: ' + parent6.data);
+console.log('Parent 7: ' + parent7.data);
+console.log();
+
+console.log('Find Successor:');
+var parent4 = tree1.findSuccessor(root1, node4);
+var parent1 = tree1.findSuccessor(root1, node1);
+var parent6 = tree1.findSuccessor(root1, node6);
+var parent7 = tree1.findSuccessor(root1, node7);
+console.log('Successor 4: ' + (parent4 ? parent4.data : '(null)'));
+console.log('Successor 1: ' + parent1.data);
+console.log('Successor 6: ' + parent6.data);
+console.log('Successor 7: ' + parent7.data);
+console.log();
+
+console.log('Remove');
+var root1 = tree1.remove(root1, node1);
+console.log('No Children 1:');
+console.log(tree1.toString(root1));
+var root1 = tree1.remove(root1, node7);
+console.log('One Child: 7');
+console.log(tree1.toString(root1));
+var root1 = tree1.remove(root1, node6);
+console.log('Two Children 6:');
+console.log(tree1.toString(root1));
+var root1 = tree1.remove(root1, root1);
+console.log('Root: 4');
+console.log(tree1.toString(root1));
+console.log();
