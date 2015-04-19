f010329758808d20374d8a9e318d58a22e956ba4
Matt Magurany <mmagurany@live.com>
2015-04-18 21:28:02 -0400

    Implement double to dollars in javascript

---
+++ javascript/to-dollar-string.js
@@ -0,0 +1,35 @@
+var number = Number(process.argv[2]);
+var numbers = ['0','1','2','3','4','5','6','7','8','9'];
+var count = 0;
+var separator = '.'
+var index = 2;
+var result = '';
+var isNegative = number < 0;
+
+number = number * 100 + 0.5;
+
+if (isNegative) {
+  number = number * -1;
+}
+
+do {
+  number = Math.floor(number);
+  var digit = Math.floor(number % 10);
+  if (count == index) {
+    result = separator + result;
+    count = 0;
+    separator = ',';
+    index = 3;
+  }
+  result = numbers[digit] + result;
+  number = number / 10;
+  count++;
+} while (number >= 1);
+
+result = '$' + result;
+
+if (isNegative) {
+  result = '(' + result + ')';
+}
+
+console.log(result);
