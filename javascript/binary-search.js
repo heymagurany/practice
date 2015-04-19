ec68c7bb26a7da83537aa007664772718116eee2
Matt Magurany <mmagurany@live.com>
2015-04-19 19:11:57 -0400

    Find in rotated array v2.

--- javascript/binary-search.js
+++ javascript/binary-search.js
@@ -1,18 +1,18 @@
 var value = process.argv[2];
-var rotation = parseInt(process.argv[3]);
+var rotate = parseInt(process.argv[3]);
 var array = ['a','b','c','d','e','f','g','h','i','j','k','l','m','n','o','p','q','r','s','t','v','w','x','y','z'];
 
-function printArray(start, end) {
+function printArray(start, mid, end) {
   var result = array.reduce(function (previousValue, currentValue, index) {
     if (index < start || index > end) {
       return previousValue + ' ';
     }
     return previousValue + currentValue;
   }, '');
-  console.log('[' + result + ']')
+  console.log('(' + array[mid] + ')[' + result + ']')
 }
 
-for (var i = 0; i < rotation; i++) {
+for (var i = 0; i < rotate; i++) {
   array.push(array.shift());
 }
 
@@ -20,27 +20,38 @@ var start = 0;
 var end = array.length - 1;
 
 while (start < end) {
-  printArray(start, end);
   var mid = start + Math.floor((end - start) / 2);
-  if (value == array[mid]) {
+  printArray(start, mid, end);
+  if (array[start] <= array[mid]) {
+    if(array[mid] < array[end]) {
+      break;
+    }
+    start = mid + 1;
+  }
+  else {
     end = mid;
-  }
-  else if (array[start] <= array[mid]) {
-    if (array[start] <= value && value < array[mid]) {
-      end = mid;
-    }
-    else {
-      start = mid + 1;
-    }
-  }
-  else if (array[mid] <= array[end]) {
-    if (array[mid] <= value && value < array[end]) {
-      start = mid + 1;
-    }
-    else {
-      end = mid;
-    }
   }
 }
 
-console.log('Index: [' + start + '] = ' + array[start]);
+var pivot = start;
+
+console.log('Pivot: [' + pivot + '] = ' + array[pivot]);
+
+var start = 0;
+var end = array.length - 1;
+
+while (start < end) {
+  var mid = start + Math.floor((end - start) / 2);
+  printArray(start, mid, end);
+  var midValue = array[Math.floor((mid + pivot) % array.length)];
+  if (value <= midValue) {
+    end = mid;
+  }
+  else {
+    start = mid + 1;
+  }
+}
+
+var result = Math.floor((start + pivot) % array.length);
+
+console.log('Index: [' + result + '] = ' + array[result]);
